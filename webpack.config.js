dff6c8bae52d180e369d9fc65159b91c2e6d85d6
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-05-28 20:54:49 +0300

    use source map

--- webpack.config.js
+++ webpack.config.js
@@ -4,45 +4,48 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ESLintPlugin = require('eslint-webpack-plugin')
 
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react']
+module.exports = (env) => {
+  return {
+    mode: env.dev ? 'development' : 'production',
+    entry: './src/index.js',
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: env.dev ? 'eval-source-map' : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-react']
+            }
           }
         }
-      }
-    ]
-  },
-  plugins: [
-    new ESLintPlugin({
-      exclude: ['node_modules', 'dist'],
-      context: path.resolve(__dirname, 'src')
-    }),
-    new HtmlWebpackPlugin({
-      template: 'public/index.html'
-    })
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'dist')
+      ]
     },
-    allowedHosts: 'all',
-    compress: false,
-    port: 3000,
-    historyApiFallback: {
-      index: 'index.html'
+    plugins: [
+      new ESLintPlugin({
+        exclude: ['node_modules', 'dist'],
+        context: path.resolve(__dirname, 'src')
+      }),
+      new HtmlWebpackPlugin({
+        template: 'public/index.html'
+      })
+    ],
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist')
+      },
+      allowedHosts: 'all',
+      compress: false,
+      port: 3000,
+      historyApiFallback: {
+        index: 'index.html'
+      }
     }
   }
 }
