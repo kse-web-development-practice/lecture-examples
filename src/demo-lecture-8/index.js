c4fd740ba08dc0c14deac1dfb21a6666b8164275
denys.zavhorodnii <denis.zavgorodny@gmail.com>
2024-06-05 20:20:52 +0300

    cleanup

--- src/demo-lecture-8/index.js
+++ src/demo-lecture-8/index.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import { Mouse, Pointer } from './render-props'
-import { withLogging } from './high-order-component'
 
 export const Lecture8 = () => {
-  const MouseWithLogging = withLogging(Mouse)
   return (
     <div>
-      <MouseWithLogging renderPointer={(left, top) => <Pointer left={left} top={top} />} />
+      <Mouse renderPointer={(left, top) => <Pointer left={left} top={top} />} />
     </div>
   )
 }
